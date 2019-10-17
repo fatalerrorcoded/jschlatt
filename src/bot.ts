6f5bb5a23fd5f7b64de6a77f51eb28b74b57800a
Martin Löffler <git@fatalerrorcoded.eu>
2019-10-17 21:25:02 +0200

    Presence, responds to mention now

--- src/bot.ts
+++ src/bot.ts
@@ -7,17 +7,33 @@ import QuoteVoice from "./quotevoice";
 const client = new Discord.Client();
 const quotevoice = new QuoteVoice("./files/sfx");
 
+let presenceTimeout: number = 0;
+const updatePresence = (check: boolean) => {
+	if (check && new Date().getTime() < presenceTimeout + (20 * 1000)) return;
+	presenceTimeout = new Date().getTime();
+	client.user.setActivity(`clips in ${client.guilds.size} servers | @${client.user.tag} help`, { type: "PLAYING" });
+};
+
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.username}`);
+	updatePresence(false);
 });
+
+client.on("guildCreate", () => updatePresence(true));
+client.on("guildDelete", () => updatePresence(true));
 
 client.on("message", (message) => {
 	if (message.author.bot) return;
-	if (message.content === "schlatty boi get here") {
+	if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
 		const channel = message.member.voiceChannel;
-		if (channel === undefined) message.reply("join a voice channel first dumbass");
+		if (channel === undefined) message.reply("join a voice channel first");
 		quotevoice.playRandom(channel);
 	}
 });
+
+if (process.env.DISCORD_TOKEN === undefined) {
+	console.error("DISCORD_TOKEN not found in environment, set it in .env");
+	process.exit(-1);
+}
 
 client.login(process.env.DISCORD_TOKEN);
